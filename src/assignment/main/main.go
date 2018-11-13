cde1aa55c96d506ae15884e88f54f6c105bfe254
wovert <wovert@126.com>
2018-11-13 08:43:56 +0800

    add number

--- src/assignment/main/main.go
+++ src/assignment/main/main.go
@@ -4,8 +4,20 @@ import (
 	"fmt"
 	"math"
 )
+
+// 水仙花数：一个三位数，其各位数字立方和等于该数本身
+func isNumber(n int) bool {
+	var i,j,k int
+	i = n % 10
+	j = (n/10) % 10
+	k = (n/100) % 10
+	sum := i*i*i + j*j*j + k*k*k
+
+	return sum == n
+}
+
 func isPrime(n int) bool {
-	fmt.Printf("sqrt:%d\n", int(math.Sqrt(float64(n))))
+	// fmt.Printf("sqrt:%d\n", int(math.Sqrt(float64(n))))
 	for i:=2; i<=int(math.Sqrt(float64(n))); i++ {
 		if n%i == 0 {
 			return false
@@ -14,11 +26,24 @@ func isPrime(n int) bool {
 	return true
 }
 
+// 阶乘
+func sum(n int) uint64 {
+
+	var s uint64 = 1
+	var sum uint64 = 0
+	for i:=1; i<=n; i++ {
+		s := s * uint64(i)
+		fmt.Printf("%d!=%v\n", i, s)
+		sum += s
+	}
+	return sum
+}
+
 func main() {
 	var n int
-
 	var m int
-	fmt.Scanf("%d%d", &n, &m)
+
+	fmt.Scanf("%d,%d", &n, &m)
 
 	for i:=n; i<m; i++ {
 		if isPrime(i) == true {
@@ -26,4 +51,14 @@ func main() {
 			continue
 		}
 	}
+
+	for i:=n; i<m; i++ {
+		if isNumber(i) == true {
+			fmt.Println("水仙花数：", i)
+		}
+	}
+
+	s := sum(n)
+	fmt.Println(s)
+
 }
