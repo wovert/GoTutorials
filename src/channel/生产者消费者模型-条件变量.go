ed1a6d7d7958ed3cea21b852baea13788a6240fc
wovert <wovert@126.com>
2020-07-11 09:33:23 +0800

    cond

--- src/channel/生产者消费者模型-条件变量.go
+++ src/channel/生产者消费者模型-条件变量.go
@@ -8,13 +8,21 @@ import (
 )
 
 var cond sync.Cond // 全局条件变量
-
+const (
+	COUNT = 3
+)
 // 生产者：发送数据端
 func producer8(out chan<- int, index int) {
-	for {
+	for { // 循环目的是一个生产者不停的进行生产
 		cond.L.Lock()			// 条件变量对应互斥加锁
-		for len(out) == 3 { // 产品区满等待消费者消费，缓冲区已经满了，为什么循环？
-			cond.Wait()		// 挂起当前协程，等待条件变量满足，被消费者唤醒
+		for len(out) == COUNT { // 产品区满等待消费者消费，缓冲区已经满了，为什么循环？
+			cond.Wait()		// 阻塞(挂起)当前协程，等待条件变量满足，被消费者唤醒
+			// 释放已掌握的互斥锁相当于 cond.L.Unlock, 两步为一个原子操作
+			// 当被唤醒，Wait()函数返回时，解除阻塞并重新获取互斥锁，相当于 cond.L.Lock()
+			// Wait()操作步骤
+			// 1. 阻塞
+			// 2. Unlock
+			// 3. lock
 		}
 		num := rand.Intn(800) // 产生随机数
 		out <- num // 写入到 channel 中 (生产)
@@ -36,7 +44,7 @@ func consumer8(in <- chan int, index int) {
 		num := <-in						// 将 channel 中的数据读走（消费）
 		fmt.Printf("----消费者%dth, 消费数据%3d，公共区域剩余%d个数据\n", index, num, len(in))
 		cond.L.Unlock()		// 消费结束，解锁互斥锁
-		cond.Signal()			// 唤醒阻塞的生产者
+		cond.Signal()			// 唤醒阻塞的生产者，单发通知，给一个等在（阻塞）在该条件变量上的 goroutine发送通知
 		time.Sleep(time.Millisecond * 500)	// 消费完休息一会，给其他协程执行机会
 	}
 }
@@ -46,8 +54,8 @@ func main() {
 	rand.Seed(time.Now().UnixNano()) 		// 设置随机种子
 	quit := make(chan bool)							// 创建用于结束通信的 channel
 
-	product := make(chan int, 3)				// 产品区（公共区）使用channel 模拟
-	cond.L = new(sync.Mutex)						// 创建互斥锁和条件变量
+	product := make(chan int, COUNT)		// 产品区（公共区）使用channel 模拟
+	cond.L = new(sync.Mutex)						// 创建互斥锁，赋值在条件变量属性上
 
 
 	// 5个生产者
@@ -56,10 +64,10 @@ func main() {
 	}
 
 
-	// 主协程消费者
+	// 3个消费者
 	for i:=1; i<4; i++{
 		go consumer8(product, i)
 	}
 
-	<-quit
+	<-quit // 主协程，不结束
 }
