c58545d38c1b7e85f77e8aaf8a8f21dce30cf395
jiang_jianmin <402766510@qq.com>
2022-01-27 14:17:00 +0800

    change

--- src/16_concurrence/channel/读写锁-rwlock.go
+++ src/16_concurrence/channel/读写锁-rwlock.go
@@ -10,14 +10,14 @@ import (
 var rwMutex3 sync.RWMutex // 锁只有一把，2个属性
 var value int // 定义全局变量，模拟共享数据
 
-func readGo2(i int) {
-		rwMutex3.RLock() // 以读模式加锁，同时不能写入数据
-		num := value // 读取数据，共享数据
-		fmt.Printf("%dth 读协程，读出：%d\n", i, num)
-		rwMutex3.RUnlock()	// 以读模式解锁
+func readGo(i int) {
+	rwMutex3.RLock() // 以读模式加锁，同时不能写入数据
+	num := value // 读取数据，共享数据
+	fmt.Printf("%dth 读协程，读出：%d\n", i, num)
+	rwMutex3.RUnlock()	// 以读模式解锁
 }
 
-func writeGo2(i int) {
+func writeGo(i int) {
 	// 生成随机数
 	num := rand.Intn(1000)
 
@@ -33,13 +33,12 @@ func main() {
 	rand.Seed(time.Now().UnixNano())
 
 	for i:=0; i<10; i++ {
-		go readGo2(i+1)
+		go writeGo(i+1)
 	}
 
 	for i:=0; i<10; i++ {
-		go writeGo2(i+1)
+		go readGo(i+1)
 	}
-
 
 	for {
 		;
